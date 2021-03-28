20a29dd8d7e6fd0bb2a63f4f969f05676b719bcf
unknown <thaivanphu.qn95@gmail.com>
2021-03-28 20:02:20 +0700

    Create a form

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 
 function App() {
   return (
-    <div className="App">
-      nothing
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-6 col-sm-offset-3">
+          <form>
+            <div className="form-group">
+              <input type="text" name="title" className="form-control no-border" placeholder="Title..." required />
+            </div>
+
+            <div className="form-group">
+              <input type="text" name="body" className="form-control no-border" placeholder="Body..." required />
+            </div>
+
+            <div className="form-group">
+              <button className="btn btn-primary col-sm-12">Save</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
