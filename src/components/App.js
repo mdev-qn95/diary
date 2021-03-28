456ce7eda6623759b57ac59aabc7959265ca42d8
unknown <thaivanphu.qn95@gmail.com>
2021-03-28 20:16:36 +0700

    Handling onChange event

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,47 @@
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-sm-6 col-sm-offset-3">
-          <form>
-            <div className="form-group">
-              <input type="text" name="title" className="form-control no-border" placeholder="Title..." required />
-            </div>
+class App extends Component {
 
-            <div className="form-group">
-              <input type="text" name="body" className="form-control no-border" placeholder="Body..." required />
-            </div>
+  constructor(props) {
+    super(props)
+    // state
+    this.state = {
+      title: '',
+      body: ''
+    }
+    // bind
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-            <div className="form-group">
-              <button className="btn btn-primary col-sm-12">Save</button>
-            </div>
-          </form>
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6 col-sm-offset-3">
+            <form>
+              <div className="form-group">
+                <input onChange={this.handleChange} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
+              </div>
+
+              <div className="form-group">
+                <input onChange={this.handleChange} type="text" name="body" className="form-control no-border" placeholder="Body..." required />
+              </div>
+
+              <div className="form-group">
+                <button className="btn btn-primary col-sm-12">Save</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
