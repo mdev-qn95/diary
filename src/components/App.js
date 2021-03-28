35893d7e2f77c8bdea569dcd8ef630f3f72afd85
unknown <thaivanphu.qn95@gmail.com>
2021-03-28 21:13:11 +0700

    Handling onSubmit and saving to firebase realtime database

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { database } from '../firebase'
 
 class App extends Component {
 
@@ -11,11 +12,27 @@ class App extends Component {
     }
     // bind
     this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  // handle change
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value
+    })
+  }
+
+  // handle submit
+  handleSubmit(e) {
+    e.preventDefault()
+    const note = {
+      title: this.state.title,
+      body: this.state.body,
+    }
+    database.push(note)
+    this.setState({
+      title: '',
+      body: ''
     })
   }
 
@@ -24,13 +41,13 @@ class App extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <div className="form-group">
-                <input onChange={this.handleChange} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
+                <input onChange={this.handleChange} value={this.state.title} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
               </div>
 
               <div className="form-group">
-                <input onChange={this.handleChange} type="text" name="body" className="form-control no-border" placeholder="Body..." required />
+                <input onChange={this.handleChange} value={this.state.body} type="text" name="body" className="form-control no-border" placeholder="Body..." required />
               </div>
 
               <div className="form-group">
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import * as firebase from 'firebase'
+import firebase from 'firebase'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
