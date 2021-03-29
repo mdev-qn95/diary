455a171fd13af9f06d5d3cc16537d2ea3aa33465
unknown <thaivanphu.qn95@gmail.com>
2021-03-29 23:23:11 +0700

    Create and read using redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { database } from '../firebase'
 import _ from 'lodash'
 import { connect } from 'react-redux'
 import { getNotes, saveNote } from '../actions/notesAction'
@@ -11,8 +10,7 @@ class App extends Component {
     // state
     this.state = {
       title: '',
-      content: '',
-      notes: {}
+      content: ''
     }
     // bind
     this.handleChange = this.handleChange.bind(this)
@@ -39,7 +37,7 @@ class App extends Component {
       title: this.state.title,
       content: this.state.content,
     }
-    this.props.saveNote()
+    this.props.saveNote(note)
     this.setState({
       title: '',
       content: ''
@@ -48,7 +46,7 @@ class App extends Component {
 
   // render notes
   renderNotes() {
-    return _.map(this.state.notes, (note, key) => {
+    return _.map(this.props.notes, (note, key) => {
       return (
         <div key="key">
           <div class="alert alert-success" role="alert">
@@ -64,22 +62,24 @@ class App extends Component {
   render() {
     return (
       <div className="container-fluid">
+        <br />
         <div className="row">
-          <div className="col-sm-6 col-sm-offset-3">
+          <div className="col-sm-5 col-xs-12">
             <form onSubmit={this.handleSubmit}>
               <div className="form-group">
                 <input onChange={this.handleChange} value={this.state.title} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
               </div>
 
               <div className="form-group">
-                <textarea onChange={this.handleChange} value={this.state.content} type="text" name="content" className="form-control no-border" placeholder="Content..." required />
+                <textarea onChange={this.handleChange} value={this.state.content} type="text" name="content" rows="15" className="form-control no-border" placeholder="Content..." required />
               </div>
 
               <div className="form-group">
                 <button className="btn btn-primary col-sm-12">Save</button>
               </div>
             </form>
-
+          </div>
+          <div className="col-sm-7 col-xs-12">
             {this.renderNotes()}
           </div>
         </div>
