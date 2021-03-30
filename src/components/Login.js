b12fa0157aae67892861b857a5fc2d74b6d13be7
unknown <thaivanphu.qn95@gmail.com>
2021-03-30 21:05:25 +0700

    Login page markup

---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row text-center">
+                    <div className="col-sm-12 jumbotron">
+                        <h1>Login with your favorite <b>Social Network</b></h1>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <button class="btn btn-danger btn-lg">Login with Google</button>
+                    </div>
+                    
+                    <div className="col-sm-6">
+                        <button class="btn btn-primary btn-lg">Login with Facebook</button>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login
