0bb575f79d02a94738e1d354d193afef52a7eb43
unknown <thaivanphu.qn95@gmail.com>
2021-03-30 21:30:52 +0700

    Building navbar

--- src/index.js
+++ src/index.js
@@ -10,17 +10,40 @@ import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Login from './components/Login'
 
 // create redux store -> reducers -> 'actions - actionType' | applyMiddleware()
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
+const Header = () => (
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <Link className="navbar-brand" to="/">DIARY</Link>
+    </div>
+  </nav>
+)
 // provide the store to react
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" component={App} exact={true} />
+            <Route path="/login" component={Login} exact={true} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
