7f10e927ea5e00053b6844be480c49b9ffbb244a
unknown <thaivanphu.qn95@gmail.com>
2021-03-31 21:25:47 +0700

    Dispatching user status

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,12 +1,22 @@
 import { auth, facebookProvider, googleProvider } from '../firebase'
-import { GET_USER } from '../actionTypes'
+import { GET_USER, USER_STATUS } from '../actionTypes'
 
 export function getUser() {
     return dispatch => {
+        // show loading status before getting user to true
+        dispatch({
+            type: USER_STATUS,
+            payload: true
+        })
         auth.onAuthStateChanged(user => {
             dispatch({
                 type: GET_USER,
                 payload: user
+            })
+            // show loading status to false
+            dispatch({
+                type: USER_STATUS,
+                payload: false
             })
         })
     }
