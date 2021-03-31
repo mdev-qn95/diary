d12addd453ce6e1729fd715232634a62b4d1b797
unknown <thaivanphu.qn95@gmail.com>
2021-03-31 21:25:10 +0700

    Dispatching notes status

--- src/actions/notesAction.js
+++ src/actions/notesAction.js
@@ -1,13 +1,24 @@
-import { GET_NOTES } from '../actionTypes'
+import { GET_NOTES, NOTES_STATUS } from '../actionTypes'
 import { database } from '../firebase'
 
 export function getNotes() {
     return dispatch => {
+        // as soon as this function fires show loading to true
+        dispatch({
+            type: NOTES_STATUS,
+            payload: true
+        })
         database.on('value', snapshot => {
             dispatch({
                 type: GET_NOTES,
                 payload: snapshot.val()
             })
+            // once notes are received show loading to false
+            dispatch({
+                type: NOTES_STATUS,
+                payload: false
+            })
+            // wait until something changes and try again
         })
     }
 }
