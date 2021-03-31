80ac965ff399927fe1a20b8f8ba0aff6d77d6055
unknown <thaivanphu.qn95@gmail.com>
2021-03-31 21:26:22 +0700

    Loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import notesReducer from './notesReducer'
 import userReducer from './userReducer'
+import loadingReducer from './loadingReducer'
 
 const rootReducer = combineReducers({
     notes: notesReducer,
-    user: userReducer
+    user: userReducer,
+    loading: loadingReducer
 })
 
 export default rootReducer
