08a43d02e27cc32e6a4aa7454ed8a29d640806b3
unknown <thaivanphu.qn95@gmail.com>
2021-04-02 22:56:01 +0700

    Rendering comments

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import SubmitComment from '../components/SubmitComment'
+import _ from 'lodash'
+import Comment from '../components/Comment'
 
 class NoteDetail extends Component {
+
+    renderComments() {
+        const { note } = this.props
+        return _.map(note.comments, (comment, key) => {
+            return <Comment key={key} id={key}>{comment.commentContent}</Comment>
+        })
+    }
     render() {
         const { note } = this.props
         return (
@@ -13,6 +22,8 @@ class NoteDetail extends Component {
                         <h1>{note.title}</h1>
                         <p>{note.content}</p>
                         <SubmitComment id={this.props.match.params.id} />
+                        {this.renderComments()}
+                        <br />
                         <Link to="/"> &#171; Back</Link>
                     </div>
                 </div>
