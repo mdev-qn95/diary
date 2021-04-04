e21ad49151b8001a627da6f240ab6b1dd616d259
unknown <thaivanphu.qn95@gmail.com>
2021-04-04 22:39:57 +0700

    Popularte note content to update

--- src/components/NoteEdit.js
+++ src/components/NoteEdit.js
@@ -3,14 +3,66 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class NoteEdit extends Component {
-    render() {
-        return (
-            <div className="container-fluid">
-                <h2>note edit page</h2>
-            </div>
-        )
+
+    constructor(props) {
+      super(props)
+      // state
+      this.state = {
+        title: this.props.note.title,
+        content: this.props.note.content
+      }
+      // bind
+      this.handleChange = this.handleChange.bind(this)
+      this.handleSubmit = this.handleSubmit.bind(this)
     }
-}
+  
+    // handle change
+    handleChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+  
+    // handle submit
+    handleSubmit(e) {
+      e.preventDefault()
+      const note = {
+        title: this.state.title,
+        content: this.state.content,
+        uid: this.props.uid
+      }
+    //   this.props.saveNote(note)
+      this.setState({
+        title: '',
+        content: ''
+      })
+    }
+  
+    render() {
+      return (
+        <div className="container-fluid">
+          <br />
+          <div className="row">
+            <div className="col-sm-5 col-xs-12">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <input onChange={this.handleChange} value={this.state.title} type="text" name="title" className="form-control no-border" placeholder="Title..." required />
+                </div>
+  
+                <div className="form-group">
+                  <textarea onChange={this.handleChange} value={this.state.content} type="text" name="content" rows="15" className="form-control no-border" placeholder="Content..." required />
+                </div>
+  
+                <div className="form-group">
+                  <button className="btn btn-primary col-sm-12">Update</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
 
 function mapStateToProps(state, ownProps) {
     return {
